c98a59a52293b67044378874e7eb0e90b0cec258
simon <simon.sokolowski@outlook.com>
2021-04-28 11:52:21 +0100

    can retain user information using urlendcoded

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 
+var urlencodedParser = express.urlencoded({extended: false});
+
 app.set("view engine", "ejs");
 app.use("/assets", express.static("assets"));
 
@@ -10,6 +12,11 @@ app.get("/", function(req, res){
 
 app.get("/contact", function(req, res){
     res.render("contact", {qs: req.query});
+}); 
+
+app.post("/contact", urlencodedParser, function(req, res){
+    console.log(req.body);
+    res.render("contact-success", {data: req.body});
 }); 
 
 app.get("/profile/:name", function(req, res){
